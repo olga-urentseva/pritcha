59dc00a0a86b154c84d7eac32e1e30f22465eee9
Olga Urentseva <lelyalelya9666@gmail.com>
2020-08-19 01:37:32 +0200

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+const path = require("path");
+const fs = require("fs");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const isDev = process.env.NODE_ENV === "development";
+
+const pageNames = fs.readdirSync("./src/views/pages/");
+
+const entryPointsForPages = pageNames
+  .filter((pageName) =>
+    fs.readdirSync(`./src/views/pages/${pageName}`).includes("script.js")
+  )
+  .reduce(
+    (acc, pageName) => ({
+      ...acc,
+      [pageName]: `./views/pages/${pageName}/script.js`,
+    }),
+    {}
+  );
+
+const entryHtmlPlugins = pageNames.map((pageName) => {
+  const chunks = ["main"];
+  if (pageName in entryPointsForPages) {
+    chunks.push(pageName);
+  }
+  return new HtmlWebpackPlugin({
+    filename: pageName + ".html",
+    template: `./views/pages/${pageName}/template.pug`,
+    chunks,
+  });
+});
+
+module.exports = {
+  context: path.resolve(__dirname, "src"),
+  mode: "development",
+  entry: {
+    main: "./main.js",
+    ...entryPointsForPages,
+  },
+  output: {
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    ...entryHtmlPlugins,
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css",
+    }),
+  ],
+  devServer: {
+    port: 4200,
+    hot: isDev,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|jpg|svg|jpeg|woff2|woff)$/i,
+        use: ["file-loader"],
+      },
+      // {
+      //   test: /\.html$/i,
+      //   loader: "html-loader",
+      // },
+      {
+        test: /\.pug$/i,
+        use: ["pug-loader"],
+      },
+    ],
+  },
+};
