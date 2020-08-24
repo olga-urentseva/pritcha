ed1d882fdaa4eaf05d8519f8b6e23164c38b350d
Olga Urentseva <lelyalelya9666@gmail.com>
2020-08-24 19:05:42 +0200

    feat(header): pop-ap menu

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,1 +1,12 @@
 import "./styles.scss";
+
+const closeButton = document.querySelector(".header__close-button");
+const hamburgerButton = document.querySelector(".header-hamburger-button");
+const header = document.querySelector(".header");
+
+hamburgerButton.addEventListener("click", () =>
+  header.classList.remove("header--collapsed")
+);
+closeButton.addEventListener("click", () =>
+  header.classList.add("header--collapsed")
+);
