08c11bf226b7a279789967840a1f0e9c2757525b
Olga Urentseva <lelyalelya9666@gmail.com>
2020-08-27 21:40:07 +0200

    feat(news): fetch news from google sheets

---
+++ src/build/fetch_newses.js
@@ -0,0 +1,34 @@
+const https = require("https");
+
+const SPREADSHEET_ID = "1BevqrPTNNPBrMB_nEsvLYk2wXD-CPiHVi3rzzoGwXq8";
+const API_KEY = process.env.GOOGLE_SPREADSHEETS_API_KEY;
+
+function fetch(url) {
+  return new Promise((resolve, reject) => {
+    https
+      .get(url, (resp) => {
+        let data = "";
+
+        resp.on("data", (chunk) => {
+          data += chunk;
+        });
+
+        resp.on("end", () => {
+          resolve(JSON.parse(data));
+        });
+      })
+      .on("error", (err) => {
+        reject(err);
+      });
+  });
+}
+
+async function fetchNewses() {
+  const { values: rows } = await fetch(
+    `https://sheets.googleapis.com/v4/spreadsheets/${SPREADSHEET_ID}/values/newses!A2:C?key=${API_KEY}`
+  );
+
+  return rows.reverse().map(([title, text, date]) => ({ title, text, date }));
+}
+
+module.exports = fetchNewses;
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
+require("dotenv").config();
+
 const path = require("path");
 const fs = require("fs");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
+const fetchNewses = require("./src/build/fetch_newses");
 
 const isDev = process.env.NODE_ENV === "development";
 
@@ -20,58 +23,68 @@ const entryPointsForPages = fs
     {}
   );
 
-const entryHtmlPlugins = fs.readdirSync("./src/pages/").map((pageName) => {
-  const chunks = ["main"];
-  if (pageName in entryPointsForPages) {
-    chunks.push(pageName);
-  }
-  return new HtmlWebpackPlugin({
-    filename: pageName + ".html",
-    template: `./pages/${pageName}/template.pug`,
-    chunks,
+module.exports = async () => {
+  const newses = await fetchNewses();
+
+  const templateParameters = {
+    news: { newses },
+    index: { newses },
+  };
+
+  const entryHtmlPlugins = fs.readdirSync("./src/pages/").map((pageName) => {
+    const chunks = ["main"];
+    if (pageName in entryPointsForPages) {
+      chunks.push(pageName);
+    }
+    return new HtmlWebpackPlugin({
+      filename: pageName + ".html",
+      template: `./pages/${pageName}/template.pug`,
+      chunks,
+      templateParameters: templateParameters[pageName] || {},
+    });
   });
-});
 
-module.exports = {
-  context: path.resolve(__dirname, "src"),
-  mode: "development",
-  entry: {
-    main: "./main.js",
-    ...entryPointsForPages,
-  },
-  output: {
-    filename: "[name].[contenthash].js",
-    path: path.resolve(__dirname, "dist"),
-  },
-  plugins: [
-    ...entryHtmlPlugins,
-    new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin({
-      filename: "[name].[contenthash].css",
-    }),
-    new CopyPlugin({
-      patterns: [{ from: "static", to: "../dist" }],
-    }),
-  ],
-  devServer: {
-    port: 4200,
-    hot: isDev,
-  },
-
-  module: {
-    rules: [
-      {
-        test: /\.scss$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-      },
-      {
-        test: /\.(png|jpg|svg|jpeg|woff2|woff)$/i,
-        use: ["file-loader"],
-      },
-      {
-        test: /\.pug$/i,
-        use: ["pug-loader"],
-      },
+  return {
+    context: path.resolve(__dirname, "src"),
+    mode: "development",
+    entry: {
+      main: "./main.js",
+      ...entryPointsForPages,
+    },
+    output: {
+      filename: "[name].[contenthash].js",
+      path: path.resolve(__dirname, "dist"),
+    },
+    plugins: [
+      ...entryHtmlPlugins,
+      new CleanWebpackPlugin(),
+      new MiniCssExtractPlugin({
+        filename: "[name].[contenthash].css",
+      }),
+      new CopyPlugin({
+        patterns: [{ from: "static", to: "../dist" }],
+      }),
     ],
-  },
+    devServer: {
+      port: 4200,
+      hot: isDev,
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.scss$/i,
+          use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        },
+        {
+          test: /\.(png|jpg|svg|jpeg|woff2|woff)$/i,
+          use: ["file-loader"],
+        },
+        {
+          test: /\.pug$/i,
+          use: ["pug-loader"],
+        },
+      ],
+    },
+  };
 };
